5f17668980bb51dd8748f1dffc65a9a558e7e44d
Jason Bock <jason@jasonbock.net>
2019-04-23 14:54:48 -0500

    OK, got the simple demo working!

---
+++ BlazingTheWeb.Server/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace BlazingTheWeb.Server
+{
+	public class Program
+	{
+		public static void Main(string[] args) => Program.BuildWebHost(args).Run();
+
+		public static IWebHost BuildWebHost(string[] args) =>
+			 WebHost.CreateDefaultBuilder(args)
+				  .UseConfiguration(new ConfigurationBuilder()
+						.AddCommandLine(args)
+						.Build())
+				  .UseStartup<Startup>()
+				  .Build();
+	}
+}
