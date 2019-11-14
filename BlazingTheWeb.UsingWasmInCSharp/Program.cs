28e2a0b8fe3865324051b7f8d82ddc908e38d1d4
Jason Bock <jason@jasonbock.net>
2019-11-13 20:43:55 -0600

    Added UsingWasmInCSharp

---
+++ BlazingTheWeb.UsingWasmInCSharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using WebAssembly.Runtime;
+
+namespace BlazingTheWeb.UsingWasmInCSharp
+{
+	public static class Program
+	{
+		static void Main(string[] args)
+		{
+			var module = Compile.FromBinary<dynamic>(
+				@"..\..\..\..\BlazingTheWeb.WebAssembly\wwwroot\collatz.wasm");
+			var functions = module(new ImportDictionary()).Exports;
+			// 10, 5, 16, 8, 4, 2, 1 => 6 iterations
+			Console.Out.WriteLine(functions.collatz(10));
+		}
+	}
+}
