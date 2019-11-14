e7bf1e861d3b073f32a0b9c2d0cdb76f620078d7
Jason Bock <jason@jasonbock.net>
2019-11-13 18:43:36 -0600

    Updated for .NET Core 3.1

---
+++ BlazingTheWeb/Startup.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Components.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlazingTheWeb
+{
+	public class Startup
+	{
+		public void ConfigureServices(IServiceCollection services) { }
+
+		public void Configure(IComponentsApplicationBuilder app) => 
+			app.AddComponent<App>("app");
+	}
+}
