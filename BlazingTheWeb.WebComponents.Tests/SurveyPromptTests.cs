47c74a22d82b1354c0c13afaa8d9276984b5a7cc
Jason Bock <jason@jasonbock.net>
2020-02-27 14:46:50 -0600

    Added component tests

---
+++ BlazingTheWeb.WebComponents.Tests/SurveyPromptTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components.Testing;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace BlazingTheWeb.WebComponents.Tests
+{
+	public static class SurveyPromptTests
+	{
+		[Test]
+		public static void Initialize()
+		{
+			var host = new TestHost();
+			var component = host.AddComponent<SurveyPrompt>();
+			var titleNode = component.Find("strong");
+			Assert.That(titleNode.InnerText, Is.EqualTo(string.Empty));
+		}
+
+		[Test]
+		public static void InitializeWithParameters()
+		{
+			var host = new TestHost();
+			var component = host.AddComponent<SurveyPrompt>(
+				new Dictionary<string, object> { { "Title", "My title" } } );
+
+			// You can't set it this way, it's a one-way binding and it won't work.
+			//component.Instance.Title = "My title";
+
+			var titleNode = component.Find("strong");
+			Assert.That(titleNode.InnerText, Is.EqualTo("My title"));
+		}
+	}
+}
