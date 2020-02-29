3b20df07ccd3aedbeb9974deda98c17edee01ae3
Jason Bock <jason@jasonbock.net>
2020-02-29 15:57:04 -0600

    Added simple Selenium example

---
+++ BlazingTheWeb.SeleniumTests/Program.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace BlazingTheWeb.SeleniumTests
+{
+	public static class Program
+	{
+		static void Main()
+		{
+			using var driver = new ChromeDriver();
+			driver.Navigate().GoToUrl("https://localhost:44383/sequence/55");
+
+			var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+			var firstResult = wait.Until(driver => driver.FindElement(By.Id("currentSequence")) is { });
+
+			var resultElement = driver.FindElement(By.Id("currentSequence"));
+			Console.Out.WriteLine(resultElement.Text);
+		}
+	}
+}
