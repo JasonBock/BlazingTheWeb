6f3f1766e2a814414eb01d3243a1780e8047b654
Jason Bock <jason@jasonbock.net>
2020-05-05 14:54:11 -0500

    Upgraded to the latest Blazor bits

--- BlazingTheWeb.WebAssemblyHost/Program.cs
+++ BlazingTheWeb.WebAssemblyHost/Program.cs
@@ -1,20 +1,27 @@
-﻿using System.Threading.Tasks;
-using Microsoft.AspNetCore.Blazor.Hosting;
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 using BlazingTheWeb.WebComponents;
 using BlazingTheWeb.WebComponents.Extensions;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BlazingTheWeb.WebAssemblyHost
 {
-	public class Program
+	public static class Program
 	{
 		public static async Task Main(string[] args)
 		{
 			var builder = WebAssemblyHostBuilder.CreateDefault(args);
 			builder.RootComponents.Add<App>("app");
+
 			builder.Services.AddWebComponents();
+			builder.Services.AddTransient(_ => new HttpClient 
+			{ 
+				BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) 
+			});
 
 			await builder.Build().RunAsync();
-		}
+		}	
 	}
 }
