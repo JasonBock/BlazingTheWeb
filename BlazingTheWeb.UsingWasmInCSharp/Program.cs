d4fb9045e0f360a465ab9fb3a32c6df603f211ef
Jason Bock <jason.r.bock@outlook.com>
2021-01-22 07:39:34 -0600

    UsingWasmInCSharp is updated

--- BlazingTheWeb.UsingWasmInCSharp/Program.cs
+++ BlazingTheWeb.UsingWasmInCSharp/Program.cs
@@ -1,41 +1,33 @@
-﻿using System;
+﻿using BlazingTheWeb.UsingWasmInCSharp;
+using System;
 using System.Collections.Generic;
 using WebAssembly.Runtime;
 
-namespace BlazingTheWeb.UsingWasmInCSharp
+RunCollatz();
+RunCollatzWithCallback();
+
+static void RunCollatz()
 {
-	public static class Program
+	var module = Compile.FromBinary<Collatz>(
+		@"..\..\..\..\BlazingTheWeb.WebAssembly\wwwroot\collatz.wasm");
+	using var instance = module(new ImportDictionary());
+	var functions = instance.Exports;
+	// 10, 5, 16, 8, 4, 2, 1 => 6 iterations
+	Console.Out.WriteLine(functions.collatz(10));
+}
+
+static void RunCollatzWithCallback()
+{
+	var values = new List<int>();
+	var module = Compile.FromBinary<CollatzWithCallback>(
+		@"..\..\..\..\BlazingTheWeb.WebAssembly\wwwroot\collatzWithCallback.wasm");
+	var imports = new ImportDictionary
 	{
-		static void Main()
-		{
-			Program.RunCollatz();
-			Program.RunCollatzWithCallback();
-		}
-
-		private static void RunCollatz()
-		{
-			var module = Compile.FromBinary<Collatz>(
-				@"..\..\..\..\BlazingTheWeb.WebAssembly\wwwroot\collatz.wasm");
-			using var instance = module(new ImportDictionary());
-			var functions = instance.Exports;
-			// 10, 5, 16, 8, 4, 2, 1 => 6 iterations
-			Console.Out.WriteLine(functions.collatz(10));
-		}
-
-		private static void RunCollatzWithCallback()
-		{
-			var values = new List<int>();
-			var module = Compile.FromBinary<CollatzWithCallback>(
-				@"..\..\..\..\BlazingTheWeb.WebAssembly\wwwroot\collatzWithCallback.wasm");
-			var imports = new ImportDictionary
-			{
-				{ "imports", "collatzCallback", new FunctionImport(new Action<int>(value => values.Add(value))) }
-			};
-			using var instance = module(imports);
-			var functions = instance.Exports;
-			// 10 => "5, 16, 8, 4, 2, 1"
-			functions.collatz(10);
-			Console.Out.WriteLine(string.Join(", ", values));
-		}
-	}
+		{ "imports", "collatzCallback", new FunctionImport(new Action<int>(value => values.Add(value))) }
+	};
+	using var instance = module(imports);
+	var functions = instance.Exports;
+	// 10 => "5, 16, 8, 4, 2, 1"
+	functions.collatz(10);
+	Console.Out.WriteLine(string.Join(", ", values));
 }
