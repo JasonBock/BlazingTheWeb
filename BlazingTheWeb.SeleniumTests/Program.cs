d29e42e1cef52b4295555b40908c14d261b2778c
Jason Bock <jason.r.bock@outlook.com>
2021-01-22 07:47:22 -0600

    SeleniumTests is updated

--- BlazingTheWeb.SeleniumTests/Program.cs
+++ BlazingTheWeb.SeleniumTests/Program.cs
@@ -3,20 +3,11 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using System;
 
-namespace BlazingTheWeb.SeleniumTests
-{
-	public static class Program
-	{
-		static void Main()
-		{
-			using var driver = new ChromeDriver();
-			driver.Navigate().GoToUrl("https://localhost:44383/sequence/55");
+using var driver = new ChromeDriver();
+driver.Navigate().GoToUrl("http://localhost:50455/sequence/55");
 
-			var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-			var firstResult = wait.Until(driver => driver.FindElement(By.Id("currentSequence")) is { });
+var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+var firstResult = wait.Until(driver => driver.FindElement(By.Id("currentSequence")) is { });
 
-			var resultElement = driver.FindElement(By.Id("currentSequence"));
-			Console.Out.WriteLine(resultElement.Text);
-		}
-	}
-}
+var resultElement = driver.FindElement(By.Id("currentSequence"));
+Console.Out.WriteLine(resultElement.Text);
