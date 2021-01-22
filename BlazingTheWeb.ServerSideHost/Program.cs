682880ff0cfa7c2ea48a240d98de607a2d8d0616
Jason Bock <jason.r.bock@outlook.com>
2021-01-22 07:34:32 -0600

    Updated ServerSideHost

--- BlazingTheWeb.ServerSideHost/Program.cs
+++ BlazingTheWeb.ServerSideHost/Program.cs
@@ -1,17 +1,12 @@
+using BlazingTheWeb.ServerSideHost;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
-namespace BlazingTheWeb.ServerSideHost
-{
-	public class Program
-	{
-		public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
+CreateHostBuilder(args).Build().Run();
 
-		public static IHostBuilder CreateHostBuilder(string[] args) =>
-			 Host.CreateDefaultBuilder(args)
-				  .ConfigureWebHostDefaults(webBuilder =>
-				  {
-					  webBuilder.UseStartup<Startup>();
-				  });
-	}
-}
+static IHostBuilder CreateHostBuilder(string[] args) =>
+	 Host.CreateDefaultBuilder(args)
+		.ConfigureWebHostDefaults(webBuilder =>
+		{
+			webBuilder.UseStartup<Startup>();
+		 });
